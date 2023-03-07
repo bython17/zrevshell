a73230a8b0300844ee471fa0fffcfecf7985c15e
Bezaleel Wondimagegn <92359779+bython17@users.noreply.github.com>
2023-03-08 00:42:16 +0300

    Improve test ideology
    Now we are going to be testing on a server_cmd level
    not for every function defined.

--- tests/configuration.py
+++ tests/configuration.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+
 from reverse_shell.server import config
 
-configuration = config.Config(config.get_argument_parser().parse_args())
+# Let's check if our base directory exists and if it doesn't
+# we'll create it.
+
+base_dir = Path(".tmp/server_data/")
+
+if not base_dir.resolve().is_dir():
+    base_dir.mkdir(parents=True)
+
+configuration = config.Config(
+    config.get_argument_parser().parse_args(["-b", f"{base_dir}"]),
+    allow_multi_thread_db_access=True,
+)
